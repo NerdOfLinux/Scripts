158e455e9102664a3ab60d4c17486069fe39dd5e
NerdOfLinux <golf@runningwind.com>
2017-01-02 15:41:31 -0600

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,12 @@
+#Update script for Ubuntu
+#Check that user is root
 if [ $(whoami) != "root" ]
 then
 	echo "Please run as root."
+	#Exit
 	exit 0
 fi
+#If $1 is empty
 if [ -z $1 ]
 then
 	echo "Updating package indexes..."
@@ -10,6 +14,7 @@ then
 	"Updating..."
 	apt upgrade -y
 	apt dist-upgrade -y
+#Else if $1 is fix
 elif [ $1 = "fix" ]
 then
 	rm /var/lib/apt/lists/lock
@@ -17,6 +22,7 @@ then
 	rm /var/lib/dpkg/lock
 	dpkg --configure -a
 	apt update
+#Else if $1 is clean
 elif [ $1 = "clean" ]
 then
 	echo "Cleaning apt cache..."
