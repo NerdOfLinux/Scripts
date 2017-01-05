0239055606ad4bd5b995dd01ff4c76b664dd16be
NerdOfLinux <golf@runningwind.com>
2017-01-04 20:49:25 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #This script makes all other scripts in this repository executable
-echo "Making all other scripts in this repository executable."
+echo "Making all other scripts in this repository executable..."
 chmod +x *.sh
 chmod +x Linuxbrew/*.sh
+echo "Done. Run ./script-name-here to run a script."
