5a6ff51ef6213d2bded326599c075371ad2d840e
NerdOfLinux <golf@runningwind.com>
2017-01-11 17:10:46 -0600

    Update clamscan.sh

--- clamscan.sh
+++ clamscan.sh
@@ -1,3 +1,20 @@
 #This script uses clamav
 if [ $(which clamscan) ]
 then
+  if [ $(whoami) = "root" ]
+  then
+    freshclam
+  fi
+  mkdir ~/.found_viruses 2> /dev/null
+  if [ -z $1 ]
+  then
+    echo "Please enter a folder to scan."
+    read $folder
+  else
+    folder=$1
+  fi
+  clamscan -r --move="~/.found_viruses" $folder
+else
+  echo "Please install clamav."
+  exit 0
+fi
