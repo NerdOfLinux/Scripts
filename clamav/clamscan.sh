55183ee547ca646ae6c803cae2b2d07448aa5427
NerdOfLinux <golf@runningwind.com>
2017-01-11 17:24:32 -0600

    Update clamscan.sh

--- clamav/clamscan.sh
+++ clamav/clamscan.sh
@@ -1,11 +1,15 @@
 #This script uses clamav
+#Chck if clamscan in installed
 if [ $(which clamscan) ]
 then
+  #Check for root user
   if [ $(whoami) = "root" ]
   then
     freshclam
   fi
-  mkdir found_viruses
+  #Make a folder
+  mkdir found_viruses 2> /dev/null
+  #If $1 is empty
   if [ -z $1 ]
   then
     echo "Please enter a folder to scan."
@@ -13,7 +17,8 @@ then
   else
     folder=$1
   fi
-  clamscan -r --move=found_viruses $folder
+  #Scan
+  clamscan -r --move=found_viruses --max-filesize=1024M  --max-scansize=1024M $folder
 else
   echo "Please install clamav."
   exit 0
