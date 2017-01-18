144824aae18fdf8f5fa85e8747df95af1b46b093
NerdOfLinux <golf@runningwind.com>
2017-01-17 21:11:54 -0600

    Update earlyoom.sh

--- earlyoom.sh
+++ earlyoom.sh
@@ -13,12 +13,7 @@ then
   #Exit
   exit 0
 fi
-#Check for git
-if [ $(which git) ]
-then
-  git clone https://github.com/rjakob/earlyoom.git
-#Else if curl is installed
-elif [ $(which curl) ]
+if [ $(which curl) ]
 then
   #Get and unzip repository
   curl https://codeload.github.com/rfjakob/earlyoom/zip/master -o earlyoom.zip
