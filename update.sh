20fea77d204515091f419b6afb874c84e2f0454f
NerdOfLinux <golf@runningwind.com>
2017-02-26 09:18:17 -0600

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,62 +6,38 @@ then
 	#Exit
 	exit 0
 fi
-#Check for apt
+#If apt is present
 if [ $(which apt) ]
 then
-	#If $1 is empty
-	if [ -z $1 ]
-	then
-		echo "Updating package indexes..."
-		apt update
-		"Updating..."
-		apt upgrade -y
-		apt dist-upgrade -y
-	#Else if $1 is fix
-	elif [ $1 = "fix" ]
-	then
-		rm /var/lib/apt/lists/lock
-		rm /var/cache/apt/archives/lock
-		rm /var/lib/dpkg/lock
-		dpkg --configure -a
-		apt update
-		apt -f -y install
-	#Else if $1 is clean
-	elif [ $1 = "clean" ]
-	then
-		echo "Cleaning apt cache..."
-		apt clean
-		apt autoclean
-		echo "Removing old/unused packages..."
-		apt autoremove -y
-	fi
-#For older systems that don't have apt
+	APT="apt"
+#Else if apt-get is present
 elif [ $(which apt-get) ]
 then
-	#If $1 is empty
-	if [ -z $1 ]
-	then
-		echo "Updating package indexes..."
-		apt-get update
-		"Updating..."
-		apt-get upgrade -y
-		apt-get dist-upgrade -y
-	#Else if $1 is fix
-	elif [ $1 = "fix" ]
-	then
-		rm /var/lib/apt/lists/lock
-		rm /var/cache/apt/archives/lock
-		rm /var/lib/dpkg/lock
-		dpkg --configure -a
-		apt-get update
-		apt-get -f -y install
-	#Else if $1 is clean
-	elif [ $1 = "clean" ]
-	then
-		echo "Cleaning apt cache..."
-		apt-get clean
-		apt-get autoclean
-		echo "Removing old/unused packages..."
-		apt-get autoremove -y
-	fi
+	APT="apt-get"
+fi
+#If $1 is empty
+if [ -z $1 ]
+then
+	echo "Updating package indexes..."
+	eval $APT update
+	"Updating..."
+	eval $APT upgrade -y
+	eval $APT dist-upgrade -y
+#Else if $1 is fix
+elif [ $1 = "fix" ]
+then
+	rm /var/lib/apt/lists/lock &
+	rm /var/cache/apt/archives/lock &
+	rm /var/lib/dpkg/lock &
+	dpkg --configure -a
+	eval $APT update
+	eval $APT -f -y install
+#Else if $1 is clean
+elif [ $1 = "clean" ]
+then
+	echo "Cleaning apt cache..."	
+	eval $APT clean
+	eval $APT autoclean
+	echo "Removing old/unused packages..."
+	eval $APT autoremove -y
 fi
