89ccc951d60ca140dc75982b2099d615d2f7f130
NerdOfLinux <golf@runningwind.com>
2017-05-20 22:22:15 -0500

    Update browser.sh

--- browser.sh
+++ browser.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+#Create function
+check_install()
+{
+    browser=$1
+    if [ $(which $browser) ]
+    then
+        install_status="True"
+    else
+        install_status="False"
+    fi
+    if [ $install_status = "True" ]
+    then
+        echo "Already installed."
+        exit
+    fi
+}
 #Check for root
 if [ $(whoami) != "root" ]
 then
@@ -12,3 +28,27 @@ then
 else
     browser=$1
 fi
+#For opera
+if [ $browser = "opera" ]
+    #Check install status  
+    check_install $browser
+    if [ $install_status = "False" ]
+        #Install
+        wget http://download2.operacdn.com/pub/opera/desktop/45.0.2552.812/linux/opera-stable_45.0.2552.812_amd64.deb
+        dpkg -i opera-stable_45.0.2552.812_amd64.deb
+    else
+        echo "Error, please try again"
+        exit
+    fi
+#Else if browser is chrome
+elif [ $browser = "crhome" ]
+    #Check install status  
+    check_install $browser
+    if [ $install_status = "False" ]
+        #Install
+        wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+        dpkg -i google-chrome-stable_current_amd64.deb
+    else
+        echo "Error, please try again"
+        exit
+    fi
