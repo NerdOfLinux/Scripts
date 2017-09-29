346fc7174e32efb89678a6625ccbe344ab791ce9
NerdOfLinux <nerdoflinux@gateblogs.com>
2017-09-29 08:30:26 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@
 echo "Making all other scripts in this repository executable..."
 chmod +x *.sh
 chmod +x */*.sh
-echo "Done. Run ./script-name-here to run a script."
+echo "Done. Run './script-name-here' to run a script."
