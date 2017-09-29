a04d2eeb2dd5ef2d5df4bf9ae18d6a473d378cea
NerdOfLinux <nerdoflinux@gateblogs.com>
2017-09-29 08:30:06 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,5 @@
 #This script makes all other scripts in this repository executable
 echo "Making all other scripts in this repository executable..."
 chmod +x *.sh
-chmod +x Linuxbrew/*.sh
-chmod +x clamac/*.sh
+chmod +x */*.sh
 echo "Done. Run ./script-name-here to run a script."
