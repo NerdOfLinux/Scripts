6001938d5ec2911fd7acb7df986069f25f727098
NerdOfLinux <nerdoflinux@gateblogs.com>
2017-10-04 10:10:03 -0500

    Update WordPress.sh

--- WordPress.sh
+++ WordPress.sh
@@ -132,3 +132,6 @@ if ( !defined('ABSPATH') )
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
 EOF
+#Get IP
+ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
+echo "Install done, finish setting up at: http://$ip or your domain name."
