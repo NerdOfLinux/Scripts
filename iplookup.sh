de31bf111b37b65bfa967bcd2d5fdeea51b62a4e
NerdOfLinux <nerdoflinux@gateblogs.com>
2017-12-06 22:09:40 -0600

    Added city

--- iplookup.sh
+++ iplookup.sh
@@ -1,40 +1,41 @@
 #!/bin/bash
 #Set cache folder
-ipcache=".ipcache"
+ipcache="/mnt/extra/.ipcache"
 #If the cache folder is not present, create
 if [ ! -d $ipcache ]
 then
-        echo "Cache folder does not exist, creating..."
-        mkdir $ipcache
+	echo "Cache folder does not exist, creating..."
+	mkdir $ipcache
 fi
 echo="echo"
 if [ -z $1 ]
 then
-        read -p "IP: " ip
+	read -p "IP: " ip
 else
-        ip="$1"
+	ip="$1"
 fi
 if [ -z $2 ]
 then
-        echo="echo"
+	echo="echo"
 elif [ $2 = "quiet" ] || [ $2 = "silent" ]
 then
-        echo="true"
+	echo="true"
 fi
 #If file is in cache
 if [ -f "$ipcache/$ip" ]
 then
-        #Use the file
-        $echo "Using cache."
-        jsonFile="$ipcache/$ip"
+	#Use the file
+	$echo "Using cache."
+	jsonFile="$ipcache/$ip"
 else
-        #Download
-        $echo "Downloading."
-        curl -s https://ipapi.co/$ip/json >> "$ipcache/$ip"
+	#Download
+	$echo "Downloading."
+	curl -s https://ipapi.co/$ip/json >> "$ipcache/$ip"
         jsonFile="$ipcache/$ip"
 fi
 json=$(cat $jsonFile)
 country=$(echo $json | jq -r .country_name)
 region=$(echo $json | jq -r .region)
 ISP=$(echo $json | jq -r .org)
-printf "Country: $country\nRegion/State: $region\nISP: $ISP\n"
+city=$(echo $json | jq -r .city)
+printf "Country: $country\nRegion/State: $region\nCity: $city\nISP: $ISP\n"
